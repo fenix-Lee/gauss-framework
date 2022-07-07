00fc7feea8a570b5fe11df4cbdb7008ca0c63164
changsu <g5r759med5>
2022-07-07 22:42:11 +0800

    save work

--- gauss-engine/gauss-engine-core/src/main/java/com/fenix/gauss/infrastructure/aspect/GaussCacheAspect.java
+++ gauss-engine/gauss-engine-core/src/main/java/com/fenix/gauss/infrastructure/aspect/GaussCacheAspect.java
@@ -1,9 +1,21 @@
 package com.fenix.gauss.infrastructure.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class GaussCacheAspect {
+
+    @Pointcut("@annotation(com.fenix.gauss.framework.GaussCache)")
+    public void cachePointcut() {}
+
+    @Around("cachePointcut()")
+    public Object cacheable(ProceedingJoinPoint joinPoint) {
+        System.out.println(joinPoint);
+        return null;
+    }
 }
